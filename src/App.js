270796c2618c296da03d685938756d5333498516
Bhargavi Rajanna <user email>
2021-01-27 16:41:42 +0400

    Initial push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <button>Increment</button>
+     <button>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
